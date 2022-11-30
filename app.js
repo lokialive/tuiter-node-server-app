f5515fbdace2b98728ee9eecd19adecc8690235c
yfqiao <qiao.yif@northeastern.edu>
2022-11-30 03:07:31 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
+import mongoose from 'mongoose'
+// const CONNECTION_STRING =
+//   process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
+// mongoose.connect(CONNECTION_STRING)
+// mongoose.connect('mongodb://localhost:27017/tuiter')
+mongoose.connect(
+  'mongodb+srv://webhw:webhw111111@cluster0.khhdtkt.mongodb.net/?retryWrites=true&w=majority',
+)
 import express from 'express'
 import cors from 'cors'
-
-import HelloController
-  from "./controllers/hello-controller.js"
-import UserController
-  from "./controllers/users/users-controller.js"
-import TuitsController
-  from "./controllers/tuits/tuits-controller.js";
-
-
-
+import HelloController from './controllers/hello-controller.js'
+import UserController from './controllers/users/users-controller.js'
+import TuitsController from './controllers/tuits/tuits-controller.js'
 const app = express()
-
 app.use(cors())
-app.use(express.json());
-TuitsController(app);
+app.use(express.json())
+TuitsController(app)
 HelloController(app)
 UserController(app)
-
-app.listen(process.env.PORT || 4000 );
+app.listen(process.env.PORT || 4000)
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,47 +1,45 @@
-import posts from "./tuits.js";
-let tuits = posts;
+// import posts from "./tuits.js";
+import * as tuitsDao from '../tuits/tuits-dao.js'
+// let tuits = posts;
 
-const createTuit = (req, res) => {
-  const newTuit = req.body;
-  newTuit._id = (new Date()).getTime()+'';
-  newTuit.likes = 0;
-  newTuit.liked = false;
-  tuits.push(newTuit);
-  res.json(newTuit);
-
+const createTuit = async (req, res) => {
+  const newTuit = req.body
+  newTuit._id = new Date().getTime() + ''
+  newTuit.likes = 0
+  newTuit.liked = false
+  tuits.push(newTuit)
+  const insertedTuit = await tuitsDao.createTuit(newTuit)
+  res.json(insertedTuit)
 }
 
-
-const findTuits  = (req, res) => {
-  res.json(tuits);
+const findTuits = async (req, res) => {
+  const tuits = await tuitsDao.findTuits()
+  res.json(tuits)
 }
 
-const updateTuit = (req, res) => {
-  const tuitdIdToUpdate = req.params.tid;
-  const updates = req.body;
-
-  tuits = tuits.map(t => String(t._id) === tuitdIdToUpdate ? {...t, ...updates} : t);
-
-  //const tuitIndex = tuits.findIndex(
-  //    (t) => t._id === tuitdIdToUpdate)
-  //tuits[tuitIndex] =
-  //    {...tuits[tuitIndex], ...updates};
-  res.sendStatus(200);
-
+const updateTuit = async (req, res) => {
+  const tuitdIdToUpdate = req.params.tid
+  const updates = req.body
+  // const tuitIndex = tuits.findIndex(
+  //   (t) => t._id === tuitdIdToUpdate)
+  // tuits[tuitIndex] =
+  //   {...tuits[tuitIndex], ...updates};
+  const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates)
+  res.json(status)
 }
 
-
-const deleteTuit = (req, res) => {
-  const tuitdIdToDelete = req.params.tid;
-  tuits = tuits.filter((t) =>
-      t._id !== tuitdIdToDelete);
-  res.sendStatus(200);
-
+const deleteTuit = async (req, res) => {
+  const tuitdIdToDelete = req.params.tid
+  const status = await tuitsDao.deleteTuit(tuitdIdToDelete)
+  tuits = tuits.filter((t) => t._id !== tuitdIdToDelete)
+  res.json(status)
 }
 
-export default (app) => {
-  app.post('/api/tuits', createTuit);
-  app.get('/api/tuits', findTuits);
-  app.put('/api/tuits/:tid', updateTuit);
-  app.delete('/api/tuits/:tid', deleteTuit);
+const TuitsController = (app) => {
+  app.post('/api/tuits', createTuit)
+  app.get('/api/tuits', findTuits)
+  app.put('/api/tuits/:tid', updateTuit)
+  app.delete('/api/tuits/:tid', deleteTuit)
 }
+
+export default TuitsController
